422add50d008d34722cf8628db4548b73a39fbac
Your name <Your email>
2024-11-05 23:20:00 +0100

    containers design

---
+++ script.js
@@ -0,0 +1,20 @@
+
+/*function searchTrips() {
+
+    const departure = document.getElementById('departure-input').value;
+    const arrival = document.getElementById('arrival-input').value;
+
+    const tripFound = findTrip(departure, arrival);
+
+    const trainIcon = document.getElementById('train-icon');
+    const noResultIcon = document.getElementById('no-result-icon');
+
+    if (tripFound) {
+        trainIcon.style.display = 'block';
+        noResultIcon.style.display = 'none';
+    } else {
+        trainIcon.style.display = 'none';
+        noResultIcon.style.display = 'block';
+    }
+}
+    */
